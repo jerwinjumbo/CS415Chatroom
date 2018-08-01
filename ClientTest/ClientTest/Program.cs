b6692d83d6eb4c1a31ba19823f1662cfaa2bfd85
jerwinjumbo <jjegabilagon@gmail.com>
2018-08-01 22:02:37 +0800

    Client/Server

---
+++ ClientTest/ClientTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTest
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            LoopConnect();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                Console.Write("Message: ");
+                string text = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(text);
+                _clientSocket.Send(buffer);
+
+                byte[] recBuf = new byte[1024];
+                int rec = _clientSocket.Receive(recBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(recBuf, data, rec);
+                Console.WriteLine("Message: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch(SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connecting...");
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected.");
+        }
+    }
+}
---
+++ ServerTest/ServerTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            SetupServer();
+            Console.ReadLine(); 
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult e)
+        {
+            Socket socket = _serverSocket.EndAccept(e);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client Connected.");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallBack), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void RecieveCallBack(IAsyncResult e)
+        {
+            Socket socket = (Socket)e.AsyncState;
+            int recieved = socket.EndReceive(e);
+            byte[] dataBuf = new byte[recieved];
+            Array.Copy(_buffer, dataBuf, recieved);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text received: " + text);
+
+            byte[] data = Encoding.ASCII.GetBytes("Client has joined.");
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallBack), socket);
+        }
+
+        private static void SendCallback(IAsyncResult e)
+        {
+            Socket socket = (Socket)e.AsyncState;
+            socket.EndSend(e);
+        }
+    }
+}
